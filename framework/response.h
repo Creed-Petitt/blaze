279dabfeb4993299dadfd8209ad3ee1d654097fc
Creed Petitt <creedpetitt@gmail.com>
2025-10-25 19:39:43 -0500

    add get_status_text function

--- framework/response.h
+++ framework/response.h
@@ -8,18 +8,16 @@ private:
     std::string body_;
 
     std::string build_response() const;
+    static std::string get_status_text(int code);
 
 public:
     Response();
 
     Response& status(int code);
-
     Response& header(const std::string& key, const std::string& value);
-
     Response& send(const std::string& text);
 
     void send_to_client(int client_fd) const;
-
     int get_status() const;
 
 };
