b83b6bda25cc7c60d0a7801e732a1660db0f12f5
Creed Petitt <creedpetitt@gmail.com>
2026-01-12 23:25:49 -0600

    implement on_read and on_write functions for Session class

--- framework/src/server.cpp
+++ framework/src/server.cpp
@@ -33,3 +33,59 @@ void Session::do_read() {
             &Session::on_read,
             shared_from_this()));
 }
+
+void Session::on_read(beast::error_code ec, const std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    // This means client closed connections
+    if (ec == http::error::end_of_stream) {
+        stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
+        return;
+    }
+
+    if (ec) {
+        std::cerr << "read error: " << ec.message() << std::endl;
+        return;
+    }
+
+    Request blaze_req;
+    blaze_req.method = std::string(req_.method_string());
+    blaze_req.set_target({req_.target().data(), req_.target().size()});
+    blaze_req.body = req_.body();
+
+    for(auto const& field : req_) {
+        blaze_req.add_header(
+            {field.name_string().data(), field.name_string().size()},
+            {field.value().data(), field.value().size()}
+        );
+    }
+
+    const std::string client_ip = stream_.socket().remote_endpoint().address().to_string();
+    std::string response_str = app_.handle_request(blaze_req, client_ip, req_.keep_alive());
+
+    const auto response_ptr = std::make_shared<std::string>(std::move(response_str));
+
+    // Write response from handler
+    net::async_write(
+        stream_,
+        net::buffer(*response_ptr),
+        [self = shared_from_this(), response_ptr](const beast::error_code code, const std::size_t bytes){
+                self->on_write(self->req_.keep_alive(), code, bytes);
+        }
+    );
+}
+
+void Session::on_write(const bool keep_alive, beast::error_code ec, const std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    if (ec) {
+        return;
+    }
+
+    if (!keep_alive) {
+        stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
+        return;
+    }
+
+    do_read();
+}
