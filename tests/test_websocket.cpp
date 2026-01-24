e0854771c49ee201399a2d87f34659342c9216c5
Creed Petitt <creedpetitt@gmail.com>
2026-01-23 22:36:12 -0600

    update websockets test

--- tests/test_websocket.cpp
+++ tests/test_websocket.cpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <vector>
 
 using namespace blaze;
 namespace net = boost::asio;
@@ -30,7 +31,9 @@ TEST_CASE("WebSocket: Connection and Echo", "[websocket]") {
     });
 
     std::thread server_thread([&]() {
-        app.listen(8888);
+        try {
+            app.listen(8888);
+        } catch (...) {}
     });
 
     // Wait for start
@@ -46,9 +49,7 @@ TEST_CASE("WebSocket: Connection and Echo", "[websocket]") {
 
         ws.handshake("127.0.0.1", "/chat");
         
-        // Give the server a moment to execute the on_open callback
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        
         CHECK(connected.load() == true);
 
         ws.write(net::buffer(std::string("Hello Blaze")));
@@ -57,12 +58,62 @@ TEST_CASE("WebSocket: Connection and Echo", "[websocket]") {
         ws.read(buffer);
 
         CHECK(boost::beast::buffers_to_string(buffer.data()) == "Echo: Hello Blaze");
-        {
-            std::lock_guard<std::mutex> lock(msg_mutex);
-            CHECK(received_msg == "Hello Blaze");
-        }
 
         ws.close(websocket::close_code::normal);
+    }
+
+    app.engine().stop();
+    if (server_thread.joinable()) server_thread.join();
+}
+
+TEST_CASE("WebSocket: Automated Broadcasting", "[websocket]") {
+    App app;
+    
+    app.ws("/broadcast", {
+        .on_open = [](auto ws) {}
+    });
+
+    std::thread server_thread([&]() {
+        try {
+            app.listen(8889);
+        } catch (...) {}
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+    SECTION("Multiple clients should receive broadcasted message") {
+        net::io_context ioc;
+        tcp::resolver resolver(ioc);
+        
+        // Connect Client 1
+        websocket::stream<tcp::socket> ws1(ioc);
+        auto const res1 = resolver.resolve("127.0.0.1", "8889");
+        net::connect(ws1.next_layer(), res1);
+        ws1.handshake("127.0.0.1", "/broadcast");
+
+        // Connect Client 2
+        websocket::stream<tcp::socket> ws2(ioc);
+        auto const res2 = resolver.resolve("127.0.0.1", "8889");
+        net::connect(ws2.next_layer(), res2);
+        ws2.handshake("127.0.0.1", "/broadcast");
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        // Use the new Dream API to broadcast
+        app.broadcast("/broadcast", "Global Alert");
+
+        // Client 1 should see it
+        boost::beast::flat_buffer b1;
+        ws1.read(b1);
+        CHECK(boost::beast::buffers_to_string(b1.data()) == "\"Global Alert\"");
+
+        // Client 2 should see it
+        boost::beast::flat_buffer b2;
+        ws2.read(b2);
+        CHECK(boost::beast::buffers_to_string(b2.data()) == "\"Global Alert\"");
+
+        ws1.close(websocket::close_code::normal);
+        ws2.close(websocket::close_code::normal);
     }
 
     app.engine().stop();
