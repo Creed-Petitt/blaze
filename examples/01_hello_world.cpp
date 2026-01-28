70e48670b25781aa362054ef75e2154fd7a7f003
Creed Petitt <creedpetitt@gmail.com>
2026-01-28 01:07:32 -0600

    add hello-world example

---
+++ examples/01_hello_world.cpp
@@ -0,0 +1,34 @@
+/**
+ * Example 01: Hello World
+ * 
+ * This example demonstrates the most basic Blaze application.
+ * Concepts:
+ * - App instance creation
+ * - Basic GET routing
+ * - Using the Response object to send text
+ * - Starting the server
+ */
+
+#include <blaze/app.h>
+
+using namespace blaze;
+
+int main() {
+    // 1. Create the application instance
+    App app;
+
+    // 2. Define a simple route
+    // Handlers are lambdas that return Async<void> (a C++20 coroutine)
+    app.get("/", [](Response& res) -> Async<void> {
+        // Send a plain text response
+        res.send("Hello from Blaze!");
+        co_return;
+    });
+
+    // 3. Start the server on port 8080
+    // This call is blocking and starts the internal thread pool
+    std::cout << "Starting server on http://localhost:8080" << std::endl;
+    app.listen(8080);
+
+    return 0;
+}
